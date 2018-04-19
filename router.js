f19a2071cab0164cbd4068c3887b558958fe1df0
2017sun <31566040+2017sun@users.noreply.github.com>
2018-04-19 16:30:35 +0800

    Update router.js

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 app.get("/loadmore",function(req,res){
-  var cuIdx=req.query.idx
-  var len=req.query.len
+  var cuIdx=req.query.index
+  var len=req.query.length
   var date=[]
   for(var i=0;i<len;i++){
   data.push("新闻"+(parseInt(cuIdx)+i))
